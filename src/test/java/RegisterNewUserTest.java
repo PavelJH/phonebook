b1532e17d2b8f0b1bc82c49a4c43151a05441fb0
Pavel <pavelkelbas@gmail.com>
2023-01-31 12:32:07 +0100

    Add negative @Test

--- src/test/java/RegisterNewUserTest.java
+++ src/test/java/RegisterNewUserTest.java
@@ -25,16 +25,27 @@ public class RegisterNewUserTest extends TestBase {
     By errorPasswordMMaxLengthBlock = By.id("password-error-maxlength"); // locator password (div, красная строчка)
     Faker faker = new Faker();
 
-    private void goToRegistrationPage() {
+    private void goToRegistrationPage() { // переход
         Assert.assertTrue(isElementPresent(loginForm));
         driver.findElement(userRegistrationLink).click();
         Assert.assertTrue(isElementPresent(registrationForm));// разобрать
     }
-    private void fillRegistrationForm(String userData, String password) {
+    private void fillRegistrationForm(String userData, String password) { // заполнения
         fillField(userData, emailField);
         fillField(password, passwordField);
         fillField(password, confirmPasswordField);
+        // Если вдруг добавиться новое поле к примеру confirmEmail, мы его может тут добавить а не писать большыми кусками кода в каждом тесте
     }
+    private void clickSingUpButton() { // Кликабельность
+        driver.findElement(loginButton).click();
+        driver.findElement(loginButton).isEnabled();
+    }
+    private void checkErrorMessage(By locator, String expectedErrorMessage) {
+        String actualErrorMessage = driver.findElement(locator).getText(); // find text error message
+        String err = "Actual error message is not equal expected";
+        Assert.assertEquals(actualErrorMessage, expectedErrorMessage, err);
+    }
+
 
     //Positive
     @Test
@@ -50,14 +61,12 @@ public class RegisterNewUserTest extends TestBase {
         */
         goToRegistrationPage(); // все что выше три строчки
         fillRegistrationForm(userData, password);
-        driver.findElement(loginButton).click();
-        String actualErrorMessage = driver.findElement(errorMessageBlock).getText(); // find text error message
+        clickSingUpButton();
         //Assert
-        String err = "Actual error message is not equal expected";
-        Assert.assertEquals(actualErrorMessage, expectedErrorMessage, err);
+        checkErrorMessage(errorMessageBlock, expectedErrorMessage); // от вынесеного 4-го метода
 
 
-        /* Incorect
+        /* Incorrect
 
         //Negative Test -1
         By actualUserRegistrationLink = By.cssSelector("[/user/forgotpassword]");// incorrect locator
@@ -70,7 +79,10 @@ public class RegisterNewUserTest extends TestBase {
         Assert.assertFalse(isElementPresent(By.xpath("//*[@type=\"button\"]")));
         */
     }
-//Negative
+
+
+
+    //Negative
     @Test
     public void registerNewUserWithInvalidData() {
         //Arrange - Данные
@@ -84,14 +96,22 @@ public class RegisterNewUserTest extends TestBase {
         //driver.findElement(registrationForm).isDisplayed();
         goToRegistrationPage();//делаем extract метод //refactor - extract metod
         fillRegistrationForm(userData, password);
-        //Assert.assertFalse(isElementClickable(loginButton));//проверем доступна ли клик ответ false
         Assert.assertFalse(isElementPresent(errorMessageBlock)); // find text error message block
+        //Assert
+        checkErrorMessage(errorEmailMessageBlock, expectedEmailErrorMessage); // от вынесеного 4-го метода
+        checkErrorMessage(errorPasswordMMaxLengthBlock, expectedPasswordErrorMessage); // от вынесеного 4-го метода// ВМЕСТО НИЖНИХ СТРОЧЕК//TODO
+
+        //Assert.assertFalse(isElementClickable(loginButton));//проверем доступна ли клик ответ false
+        /*
         String actualEmailErrorMessage = driver.findElement(errorEmailMessageBlock).getText();
         String actualPasswordErrorMessage = driver.findElement(errorPasswordMMaxLengthBlock).getText(); // свкрху берем ее
+
         //Assert
+        //Это 4-й метод
         String err = "Actual error message is not equal expected";
         Assert.assertEquals(actualEmailErrorMessage, expectedEmailErrorMessage, err);// notEquals
         Assert.assertEquals(actualPasswordErrorMessage, expectedPasswordErrorMessage, err);// notEquals
+        */
     }
 
 
@@ -107,10 +127,11 @@ public class RegisterNewUserTest extends TestBase {
         //Act - Действие
         goToRegistrationPage();
         fillRegistrationForm(userData, password);
-        driver.findElement(loginButton).click();
-        String actualErrorMessage = driver.findElement(errorMessageBlock).getText(); // find text error message
+        /*driver.findElement(loginButton).click();
+        driver.findElement(loginButton).isEnabled(); // кликабельна ли кнопка
+        */
+        clickSingUpButton(); // две верхние строчки
         //Assert
-        String err = "Actual error message is not equal expected";
-        Assert.assertEquals(actualErrorMessage, expectedErrorMessage, err);
+        checkErrorMessage(errorMessageBlock, expectedErrorMessage); // от вынесеного 4-го метода
     }
 }
